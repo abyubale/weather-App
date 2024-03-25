897089ca76f7ffb7692f94cb02da4888f21feeb7
abyubale <abyubale@gmail.com>
2024-03-25 20:40:41 +0530

    fix

--- src/services/getWeatherData.js
+++ src/services/getWeatherData.js
@@ -2,7 +2,7 @@ export const getWeatherData = (cityName = "mumbai") => {
   return fetch(
     `https://weather-app-backend-1n7x.onrender.com/api/weather-details?city=${cityName}`
   ).then((res) => {
-    return res.json();
+    return res;
   });
 };
 
